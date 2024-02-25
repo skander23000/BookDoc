c222e35e10b06526f105210914d7986fededf274
skander23000 <skanderchouiter@gmail.com>
2024-02-25 16:28:05 +0100

    First basic implementation Frontend/Backend

---
+++ Doctor-Appointment-Booking-Web-Nextjs/app/(route)/search/[cname]/page.js
@@ -0,0 +1,32 @@
+"use client";
+import DoctorList from "@/app/_components/DoctorList";
+import GlobalApi from "@/app/_utils/GlobalApi";
+import React, { useEffect, useState } from "react";
+
+function Search({ params }) {
+  const [doctorList, setDoctorList] = useState([]);
+  useEffect(() => {
+    console.log(params.cname);
+    getDoctors();
+  }, []);
+
+  const getDoctors = () => {
+    const request = GlobalApi.getDoctorByCategory(params.cname);
+    console.log(request); // Check if this logs a promise
+    request
+      .then((resp) => {
+        setDoctorList(resp.data.data);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch doctors:", error);
+      });
+  };
+
+  return (
+    <div className="mt-5">
+      <DoctorList heading={params.cname} doctorList={doctorList} />
+    </div>
+  );
+}
+
+export default Search;
---
+++ Doctor-Appointment-Booking-Web-Nextjs/app/_components/Header.jsx
@@ -0,0 +1,126 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  LoginLink,
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRole } from "../_context/RoleContext";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+function Header() {
+  const { role } = useRole();
+  const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Explore",
+      path: "/explore",
+    },
+    {
+      id: 3,
+      name: "Contact Us",
+      path: "/",
+    },
+  ];
+
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+  return (
+    <div
+      className="flex items-center 
+    justify-between p-4 shadow-sm md:px-20"
+    >
+      <div className="flex items-center gap-10">
+        <Image src="/logo.svg" alt="logo" width={180} height={80} />
+        <ul className="md:flex gap-8 hidden">
+          {Menu.map((item, index) => (
+            <Link href={item.path} key={index}>
+              <li
+                className="hover:text-primary
+                    cursor-pointer hover:scale-105
+                    transition-all ease-in-out"
+              >
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      {user ? (
+        <Popover>
+          <PopoverTrigger>
+            {user?.picture ? (
+              <Image
+                src={user?.picture}
+                alt="profile-image"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            ) : (
+              <Image
+                src={
+                  "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                }
+                alt="profile-image"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            )}
+          </PopoverTrigger>
+          <PopoverContent className="w-44">
+            <ul className="flex  flex-col gap-2">
+              {user == "doctor" ? (
+                <Link
+                  href={"/my-booking"}
+                  className="cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md"
+                >
+                  My Booking
+                </Link>
+              ) : (
+                <Link
+                  href={"/edit-profile"}
+                  className="cursor-pointer
+           hover:bg-slate-100 p-2 rounded-md"
+                >
+                  Edit profile
+                </Link>
+              )}
+              <li
+                className="cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md"
+              >
+                <LogoutLink> Logout </LogoutLink>
+              </li>
+            </ul>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <LoginLink>
+          {" "}
+          <Button>Get Started</Button>
+        </LoginLink>
+      )}
+    </div>
+  );
+}
+
+export default Header;
